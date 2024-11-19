0442b5680f0233fe8f51530e79daf67ac37aaff6
M-M-I-V <neiljaylacandazo@gmail.com>
2024-11-19 13:32:27 +0800

    Initial commit

---
+++ UpDown.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UpDown {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		
+		boolean exit = false;
+
+		while(!exit) {
+			System.out.print("Input first number: ")
+		}
+	}
+}
